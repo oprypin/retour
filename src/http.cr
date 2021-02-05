648ec415e6b72afd2297ddb95ef8fdd842cc151f
Oleh Prypin <oleh@pryp.in>
2021-02-06 00:27:41 +0100

    Make the code output error-friendly

--- src/http.cr
+++ src/http.cr
@@ -13,20 +13,20 @@ module Retour
       macro finished
         {% for httpm in [Retour::Get, Retour::Post, Retour::Put, Retour::Patch, Retour::Delete, Retour::Link, Retour::Unlink, Retour::Head] %}
           Retour.routes({
-            {% for method in @type.methods %}{% for annot in method.annotations(httpm) %}
+            {% for method in @type.methods %}{% for annot in method.annotations(httpm) %}\
               {{ annot[0] }} => {{ method.name.id }},
-            {% end %}{% end %}
+            {% end %}{% end %}\
           } of String => String, default: "[^/]+?", method: _{{ httpm.name.split("::")[-1].downcase.id }})
         {% end %}
       end
 
       def call(method : String, path : String, *args, **kwargs)
         case method
-          {% for httpm in %w[Get Post Put Patch Delete Link Unlink Head] %}
-            when {{ httpm.upcase }}
-              _{{ httpm.downcase.id }}(path, *args, **kwargs)
-          {% end %}
-          else
+        {% for httpm in %w[Get Post Put Patch Delete Link Unlink Head] %}\
+        when {{ httpm.upcase }}
+          _{{ httpm.downcase.id }}(path, *args, **kwargs)
+        {% end %}\
+        else
           raise Retour::NotFound.new(path)
         end
       end
